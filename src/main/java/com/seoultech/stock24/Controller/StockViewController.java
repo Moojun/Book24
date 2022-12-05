498aa6159c9e1d21144e08899da9e1055c57f881
Moojun <mgim32137@gmail.com>
2022-12-05 15:51:32 +0900

    feat: 웹크롤링 구현 #7

--- src/main/java/com/seoultech/stock24/Controller/StockViewController.java
+++ src/main/java/com/seoultech/stock24/Controller/StockViewController.java
@@ -1,5 +1,10 @@
 package com.seoultech.stock24.Controller;
 
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.Connection;
+import org.jsoup.select.Elements;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -8,7 +13,11 @@ import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.PrintWriter;
-import java.sql.*;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+// import java.sql.Connection; // In Java, cannot import two different classes with the same name.
 import java.util.Properties;
 
 @WebServlet("/stock/view")
@@ -22,6 +31,7 @@ public class StockViewController extends HttpServlet {
         response.setContentType("text/html; charset=UTF-8");
 
         String stockName = request.getParameter("stockName");
+        String stockCode = "";
 
         String resource = "db.properties";
         Properties properties = new Properties();
@@ -36,7 +46,7 @@ public class StockViewController extends HttpServlet {
             String dbPassword = properties.getProperty("password");
             String sql = "SELECT * FROM stock_kr WHERE name = ?";
 
-            Connection con = DriverManager.getConnection(dbURL, dbID, dbPassword);
+            java.sql.Connection con = DriverManager.getConnection(dbURL, dbID, dbPassword);
             PreparedStatement pst = con.prepareStatement(sql);
             pst.setString(1, stockName);
             ResultSet rs = pst.executeQuery();
@@ -49,11 +59,42 @@ public class StockViewController extends HttpServlet {
                 script.println("</script>");
             }
             else {
-                System.out.println("wow! succeed");
+                stockCode = rs.getString("code");
             }
 
         } catch (ClassNotFoundException | SQLException e) {
             throw new RuntimeException(e);
+        }
+
+        // 웹 크롤링
+        String URL = "https://finance.naver.com/item/main.naver?code=";
+        URL = URL + stockCode;
+        Connection connection = Jsoup.connect(URL);
+        try {
+            Document document = connection.get();
+            Elements elements = document.select(".date");
+            String[] str = elements.text().split(" ");
+            System.out.println("str[0] : " + str[0]);
+            System.out.println("str[1] : " + str[1]);
+
+            Elements currentList = document.select(".new_totalinfo dl>dd");
+
+            /* test
+            for (int i = 0; i < currentList.size(); i++) {
+                System.out.println(currentList.get(i).text());
+            } */
+
+            String currentDate = str[0];
+            String currentTime = str[1];
+            String currentPrice = currentList.get(3).text().split(" ")[1];  // 현재가
+
+            System.out.println("종목명 : " + stockName);
+            System.out.println("주가:" + currentPrice);
+            System.out.println("가져오는 시간:" + currentDate +" / " + currentTime);
+        }
+        catch (IOException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
         }
 
     }
